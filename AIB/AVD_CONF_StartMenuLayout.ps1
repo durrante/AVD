e450590998ad3f19892d7fb165e861d77f13e44f
durrante <64601521+durrante@users.noreply.github.com>
2022-02-09 11:15:22 +0000

    Create AVD_CONF_StartMenuLayout.ps1

---
+++ AIB/AVD_CONF_StartMenuLayout.ps1
@@ -0,0 +1,1 @@
+Import-StartLayout -LayoutPath https://letsconfigmgr998.blob.core.windows.net/applications/Windows%20Customisations/Start%20Menu%20Layout/startlayout.xml -MountPath $env:SystemDrive\
