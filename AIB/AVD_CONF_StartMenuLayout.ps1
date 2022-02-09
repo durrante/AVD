9f00acb0a6c6876922cd5b6b7e1e464f885cc02c
durrante <64601521+durrante@users.noreply.github.com>
2022-02-09 16:56:21 +0000

    Update AVD_CONF_StartMenuLayout.ps1

--- AIB/AVD_CONF_StartMenuLayout.ps1
+++ AIB/AVD_CONF_StartMenuLayout.ps1
@@ -1,1 +1,1 @@
-Import-StartLayout -LayoutPath https://letsconfigmgr998.blob.core.windows.net/applications/Windows%20Customisations/Start%20Menu%20Layout/startlayout.xml -MountPath $env:SystemDrive\
+Import-StartLayout -LayoutPath c:\temp\startmenu.xml -MountPath $env:SystemDrive\
