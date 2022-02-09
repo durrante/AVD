695969370a1971e9d2faa773655e41ada622ba2c
durrante <64601521+durrante@users.noreply.github.com>
2022-02-09 15:11:25 +0000

    Update AVD_APP_InstallIBMNotesClient.ps1

--- AIB/AVD_APP_InstallIBMNotesClient.ps1
+++ AIB/AVD_APP_InstallIBMNotesClient.ps1
@@ -1,7 +1,7 @@
 ## Installs latest version of IBM Notes from Azure Storage Account via PowerShell
 
  # Download and install IBM Notes Client
- write-host 'AIB Customization: Downloading IBM Notes Client'
+ write-host 'AIB Customisation: Downloading IBM Notes Client'
  $appName = 'IBMNotesClient'
  $drive = 'C:\Temp'
  New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
@@ -10,17 +10,17 @@
  $IBMNotesClientURL = 'https://letsconfigmgr998.blob.core.windows.net/applications/IBM%20Notes/Notes%209.0.1%20client.zip'
  $IBMNotesClientexe = 'IBMClientInstall.zip'
  $outputPath = $LocalPath + '\' + $IBMNotesClientexe
- Invoke-WebRequest -Uri $IBMNotesClientURL -OutFile $outputPath
+ Invoke-WebRequest -Uri $IBMNotesClientURL -OutFile $outputPath -UseBasicParsing
  Expand-Archive -LiteralPath 'C:\\Temp\\IBMNotesClient\\IBMClientInstall.zip' -DestinationPath $Localpath -Force -Verbose
  $FullPath = $drive + '\' + $appName + '\' + 'Notes 9.0.1 client\setup.exe'
- write-host 'AIB Customization: Starting Install of IBM Notes Client'
+ write-host 'AIB Customisation: Starting Install of IBM Notes Client'
  Start-Process -FilePath $FullPath -ArgumentList '/s', '/v"SETMULTIUSER=1 /qn /norestart"' -Wait
  Start-Sleep 2
- write-host 'AIB Customization: Finished Installation of IBM Notes Client'
+ write-host 'AIB Customisation: Finished Installation of IBM Notes Client'
 
  # Download and Install IBM Notes
 
- write-host 'AIB Customization: Downloading IBM Notes'
+ write-host 'AIB Customisation: Downloading IBM Notes'
  $appName = 'IBMNotesClient'
  $drive = 'C:\Temp'
  New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
@@ -29,11 +29,11 @@
  $IBMNotesURL = 'https://letsconfigmgr998.blob.core.windows.net/applications/IBM%20Notes/notes901FP7_win.exe'
  $IBMNotesexe = 'notes901FP7_win.exe'
  $outputPath = $LocalPath + '\' + $IBMNotesexe
- Invoke-WebRequest -Uri $IBMNotesURL -OutFile $outputPath
- write-host 'AIB Customization: Starting Install of IBM Notes'
+ Invoke-WebRequest -Uri $IBMNotesURL -OutFile $outputPath -UseBasicParsing
+ write-host 'AIB Customisation: Starting Install of IBM Notes'
  Start-Process -FilePath $Outputpath -ArgumentList '-s', '-a', '/s', '/v"/qn /norestart"' -Wait
  Start-Sleep 2
- write-host 'AIB Customization: Finished Installation of IBM Notes'
+ write-host 'AIB Customisation: Finished Installation of IBM Notes'
 
 # Cleanup temp directory
 Set-location "c:\"
