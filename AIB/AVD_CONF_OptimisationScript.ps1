1709036d5f25b50ee29bcb87e1353a576432d285
durrante <64601521+durrante@users.noreply.github.com>
2022-02-10 18:03:51 +0000

    Update AVD_CONF_OptimisationScript.ps1

--- AIB/AVD_CONF_OptimisationScript.ps1
+++ AIB/AVD_CONF_OptimisationScript.ps1
@@ -11,7 +11,7 @@
  Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
  write-host 'AIB Customization: Starting OS Optimizations script'
  Expand-Archive -LiteralPath 'C:\\Optimize\\Windows_10_VDI_Optimize-master.zip' -DestinationPath $Localpath -Force -Verbose
- Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force -Verbose
  Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-main
  
  # instrumentation
