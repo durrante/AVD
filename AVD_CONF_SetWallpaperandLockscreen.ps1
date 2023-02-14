17b4fc609bf4ecc1d98fe41e4549ce780d664fc2
durrante <64601521+durrante@users.noreply.github.com>
2023-02-14 17:25:52 +0000

    Add files via upload

---
+++ AVD_CONF_SetWallpaperandLockscreen.ps1
@@ -0,0 +1,26 @@
+New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP -Force
+
+#Variable Creation
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+$BackgroundImageURL = 'https://stlcmaibuks001.blob.core.windows.net/azureimagebuilder/Images/GradientDesktopBackground.jpg'
+$LockscreenImageURL = 'https://stlcmaibuks001.blob.core.windows.net/azureimagebuilder/Images/LogoBackGround_Tiny.jpg'
+$ImageDestinationFolder = "c:\intune"
+$Backgroundimage = "$ImageDestinationFolder\background.jpg"
+$LockScreenImage = "$ImageDestinationFolder\LockScreen.jpg"
+
+#Create image directory
+md $ImageDestinationFolder -erroraction silentlycontinue
+
+#Download image file
+Start-BitsTransfer -Source $BackgroundImageURL -Destination "$Backgroundimage"
+Start-BitsTransfer -Source $LockscreenImageURL -Destination "$LockScreenimage"
+
+#Lockscreen Registry Keys
+New-ItemProperty -Path $RegPath -Name LockScreenImagePath -Value $LockScreenImage -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $RegPath -Name LockScreenImageUrl -Value $LockScreenImage -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $RegPath -Name LockScreenImageStatus -Value 1 -PropertyType DWORD -Force | Out-Null
+
+#Background Wallpaper Registry Keys
+New-ItemProperty -Path $RegPath -Name DesktopImagePath -Value $backgroundimage -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $RegPath -Name DesktopImageUrl -Value $backgroundimage -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $RegPath -Name DesktopImageStatus -Value 1 -PropertyType DWORD -Force | Out-Null
